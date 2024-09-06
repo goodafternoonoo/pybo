35a7b05f079d9d67624bd5b8678f9639fd61c485
noonoo <chw3448@gmail.com>
2024-09-06 17:18:53 +0900

    DNS 설정

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -6,12 +6,12 @@ from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 
-logger = logging.getLogger('pybo')
+logger = logging.getLogger(__name__)
 
 
 def index(request):
     logger.info('INFO 레벨로 출력')
-    
+
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
